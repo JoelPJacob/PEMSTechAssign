f1555dd2e069cf6efff83c7a788e3d74ff3fadd2
joel <joel.jacob@roadelabs.com>
2024-10-09 17:22:52 +0530

    Added README.md with project overview, setup instructions, features, and challenges

--- src/config/api.js
+++ src/config/api.js
@@ -2,12 +2,21 @@ import axios from 'axios';
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
+const handleError = (error) => {
+
+  if (error.response.status === 500) {
+    return 'Internal Server Error: Something went wrong on the server.';
+  }
+  return 'An error occurred. Please try again later.';
+};
+
 export const getUsers = async () => {
   try {
     const response = await axios.get(`${BASE_URL}/users`);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to fetch users');
+    const errorMessage = handleError(error);
+    throw new Error(errorMessage);
   }
 };
 
@@ -16,6 +25,7 @@ export const getUserPosts = async (userId) => {
     const response = await axios.get(`${BASE_URL}/posts?userId=${userId}`);
     return response.data;
   } catch (error) {
-    throw new Error('Failed to fetch posts');
+    const errorMessage = handleError(error);
+    throw new Error(errorMessage);
   }
 };
